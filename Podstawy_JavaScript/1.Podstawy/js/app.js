fe87693faefb79f0bec56e6ab801e1f2e0d69794
Agata <agatablue@gmail.com>
2016-01-21 11:56:29 +0100

    update function exercises

--- Podstawy_JavaScript/1.Podstawy/js/app.js
+++ Podstawy_JavaScript/1.Podstawy/js/app.js
@@ -2,12 +2,4 @@
  * Created by Jacek on 2016-01-11.
  */
 
-    var name = "Monika";
-
-    function getName() {
-        var name = "Kulfon";
-        return name;
-    }
-
-    console.log(name);
 
