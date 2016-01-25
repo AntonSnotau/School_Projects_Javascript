d19a22746f75c986827e34b8d1338efab0976ce5
CodersLab <jacek.tchorzewski@coderslab.pl>
2016-01-25 14:26:02 +0100

    Added slider to workshops

--- Warsztaty/1.Slider/js/app.js
+++ Warsztaty/1.Slider/js/app.js
@@ -3,29 +3,4 @@
  */
 
 document.addEventListener("DOMContentLoaded", function(){
-    var prevButton = document.getElementById("prevPicture");
-    var nextButton = document.getElementById("nextPicture");
-    var pictures = document.querySelectorAll(".slider li");
-    var currentPicture = 0;
-
-    //pictures[currentPicture].style.display = "inline-block";
-    pictures[currentPicture].classList.add("visible");
-
-    nextButton.addEventListener("click", function(event){
-        pictures[currentPicture].classList.remove("visible");
-        currentPicture++;
-        if(currentPicture >= pictures.length){
-            currentPicture = 0;
-        }
-        pictures[currentPicture].classList.add("visible");
-    });
-
-    prevButton.addEventListener("click", function(event){
-        pictures[currentPicture].classList.remove("visible");
-        currentPicture--;
-        if(currentPicture < 0){
-            currentPicture = pictures.length - 1;
-        }
-        pictures[currentPicture].classList.add("visible");
-    });
 });
