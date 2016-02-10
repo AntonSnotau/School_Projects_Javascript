90aa24ff09178be107742b347a854a4b38fd1394
Agata <agatablue@gmail.com>
2016-02-10 11:54:36 +0100

    change blocks to block

--- 2.DOM/1.Element/js/app.js
+++ 2.DOM/1.Element/js/app.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function(){
     var homeElement = document.getElementById("home");
     var childElements = document.querySelector(".oferts").children;
     var banner = document.querySelector(".ban");
-    var blocks = document.querySelectorAll(".blocks");
+    var blocks = document.querySelectorAll(".block");
     var links = document.querySelector(".links").children;
 
     /*
