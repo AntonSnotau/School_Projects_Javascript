4a500b49e3996ec8e96171111b1a9c34210a7b59
CodersLab <jacek.tchorzewski@coderslab.pl>
2016-02-17 11:05:20 +0100

    Added some exercises to 6.Zaawansowane funkcje

---
+++ 1.Podstawy_JavaScript/6.Zaawansowane_funkcje/js/zadanie1.js
@@ -0,0 +1,35 @@
+/**
+ * Zmienne globalne i lokalne. Pamiêtasz przenoszenie instrukcji?
+ * PrzeœledŸ krok po kroku ka¿d¹ linijkê kodu. Ka¿d¹!
+ * Spróbuj napisaæ komentarze dla ka¿dej z linijek kodu
+ */
+
+
+//Twój komentarz ...
+var someGlobal = 3;
+
+//Twój komentarz ...
+function someFunction1() {
+
+    //Twój komentarz ...
+    console.log(someGlobal);
+
+    //Twój komentarz ...
+    var someGlobal = 5;
+
+    //Twój komentarz ...
+    console.log(someGlobal);
+}
+
+//Twój komentarz ...
+function someFunction2() {
+
+    //Twój komentarz ...
+    console.log(someGlobal);
+}
+
+//Twój komentarz ...
+someFunction1();
+
+//Twój komentarz ...
+someFunction2();
