7c70ccff32a5f38ce36c1d49088d0c4845dc92bf
CodersLab <jacek.tchorzewski@coderslab.pl>
2016-04-07 17:02:52 +0200

    Fixxed error in tsk

--- 1.Podstawy_JavaScript/1.Podstawy/js/cwiczenie4.js
+++ 1.Podstawy_JavaScript/1.Podstawy/js/cwiczenie4.js
@@ -13,7 +13,7 @@ function createArray(number){
     return newArray[2];
 }
 
-console.log("tablica z liczbami do 6 = ". createArray(6));
-console.log("tablica z liczbami do 1 = ". createArray(1));
-console.log("Test dla liczby ujemnej (powinna być pusta tablica) ". createArray(-6));
-console.log("Test dla zera (powinna być pusta tablica) ". createArray(0));
+console.log("tablica z liczbami do 6 = " + createArray(6));
+console.log("tablica z liczbami do 1 = " + createArray(1));
+console.log("Test dla liczby ujemnej (powinna być pusta tablica) " + createArray(-6));
+console.log("Test dla zera (powinna być pusta tablica) " + createArray(0));
